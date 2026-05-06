deb62b4fa14c5740d6f0b2429f4b4c55e13e2e5d
Adarsh Mani <136229331+frostAdarsh@users.noreply.github.com>
2026-05-06 19:24:48 +0530

    Refactor main.py to implement async lifespan management and clean up comments

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,36 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager 
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
+
 load_dotenv()
 
-# Import your database and routes
+
 from app.core.database import connect_to_mongo, close_mongo_connection
 from app.routes import upload
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+   
+    await connect_to_mongo() 
+    
+    yield 
+    
+   
+    await close_mongo_connection() 
+
 
 app = FastAPI(
     title="AI-Powered Document & Multimedia Q&A API",
     description="Backend for SDE-1 Assignment",
-    version="1.0.0"
+    version="1.0.0",
+    lifespan=lifespan 
 )
 
-# Configure CORS for the frontend
+
 origins = os.getenv("BACKEND_CORS_ORIGINS", '["http://localhost:5173"]').strip("][").replace('"', '').split(", ")
 
 app.add_middleware(
@@ -27,11 +41,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Database Connection Events
-app.add_event_handler("startup", connect_to_mongo)
-app.add_event_handler("shutdown", close_mongo_connection)
 
-# Include the API Routes (Views)
+
+
 app.include_router(upload.router, prefix="/api/v1", tags=["Files"])
 
 @app.get("/")
